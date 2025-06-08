35c99600ccc308aa07c3c009bcf7f4904d22e12b
Nickx58 <nikhilx58@gmail.com>
2025-06-08 13:18:18 +0530

    recursion paramter and function way

--- recursion/easy/sum.cpp
+++ recursion/easy/sum.cpp
@@ -7,7 +7,18 @@ int sum(int n) {
     }
     return n+sum(n-1);
 }
+
+void sum2(int n, int sum) {
+    if(n < 1) {
+        cout<<sum<<" ";
+        return;
+    }
+    sum2(n-1,sum + n);
+}
 int main() {
     int num = 5;
-    cout<<sum(num);
+    cout<<sum(num)<<endl;
+    sum2(num,0);
 }
+
+// paramter way and function return way
