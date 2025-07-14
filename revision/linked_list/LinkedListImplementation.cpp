bdaa5f13b94000447e3aa89901739fe500fc1ed6
Nickx58 <nikhilx58@gmail.com>
2025-07-14 23:44:39 +0530

    LinkedList: revision

---
+++ revision/linked_list/LinkedListImplementation.cpp
@@ -0,0 +1,47 @@
+/*
+This file will contain all the basics operation of linked list like traveral, insertion and deletion
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node *addElementFromArray(vector<int> arr)
+{
+    Node *head = new Node(arr[0]);
+
+    Node *curr = head;
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        curr->next = temp;
+        curr = temp;
+    }
+    return head;
+}
+int main()
+{
+    vector<int> arr = {1, 3, 4, 5, 6, 7, 8};
+
+    Node *head = addElementFromArray(arr);
+    Node *temp = head;
+    while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
