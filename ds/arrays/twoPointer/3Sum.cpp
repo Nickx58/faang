908b6ec5f55e7a9d68449ead0a93a8033c3bdae2
Nickx58 <nikhilx58@gmail.com>
2026-01-28 19:23:41 +0530

    Array: Three Sum

---
+++ ds/arrays/twoPointer/3Sum.cpp
@@ -0,0 +1,45 @@
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+        int n = nums.size();
+
+        for (int i = 0; i < n - 2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right)
+            {
+                int target = nums[i] + nums[left] + nums[right];
+                if (target == 0)
+                {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1])
+                    {
+                        left++;
+                    }
+                }
+                else if (target > 0)
+                {
+                    right--;
+                }
+                else
+                {
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+};
