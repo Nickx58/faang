d1cf741fd93655c317d9cc039d378985435943d3
Nickx58 <nikhilx58@gmail.com>
2026-02-11 09:58:26 +0530

    Sliding Window: Max Subarray sum of distnict element 2nd solution

--- ds/arrays/slidingWindow/maxSumDistnictSizeK.cpp
+++ ds/arrays/slidingWindow/maxSumDistnictSizeK.cpp
@@ -30,3 +30,40 @@ public:
         return maxSum;
     }
 };
+
+// alternative code
+
+class Solution
+{
+public:
+    long long maximumSubarraySum(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> freq;
+        long long window_sum = 0, maxSum = 0;
+        int left = 0;
+
+        for (int right = 0; right < nums.size(); right++)
+        {
+            window_sum += nums[right];
+            freq[nums[right]]++;
+
+            // Keep window size exactly k
+            if (right - left + 1 > k)
+            {
+                window_sum -= nums[left];
+                freq[nums[left]]--;
+                if (freq[nums[left]] == 0)
+                    freq.erase(nums[left]);
+                left++;
+            }
+
+            // Check distinct condition
+            if (right - left + 1 == k && freq.size() == k)
+            {
+                maxSum = max(maxSum, window_sum);
+            }
+        }
+
+        return maxSum;
+    }
+};
