ac6ecb8ed0e230fc5f7af2bed49b121a0f3a2b6a
Nickx58 <nikhilx58@gmail.com>
2026-02-13 13:27:05 +0530

    Sliding Window: Max Avg Subarray

---
+++ ds/arrays/slidingWindow/maxAvgSubArray.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    double findMaxAverage(vector<int> &nums, int k)
+    {
+        double sum = 0;
+        for (int i = 0; i < k; i++)
+        {
+            sum += nums[i];
+        }
+
+        double maxi = sum;
+
+        for (int i = k; i < nums.size(); i++)
+        {
+            // expand
+            sum += nums[i];
+            // shrink
+            sum -= nums[i - k];
+            maxi = max(sum, maxi);
+        }
+        return maxi / k;
+    }
+};
