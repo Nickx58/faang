75f2499e1ee989812602164d8b54844a95dc7ef6
Nickx58 <nikhilx58@gmail.com>
2026-02-24 09:39:29 +0530

    Sort three colors

---
+++ ds/arrays/twoPointer/sortThreeColors.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int i = 0;
+        int k = 0;
+        int j = nums.size() - 1;
+
+        while (k <= j)
+        {
+            if (nums[k] == 1)
+            {
+                k++;
+            }
+            else if (nums[k] == 0)
+            {
+                swap(nums[i], nums[k]);
+                i++;
+                k++;
+            }
+            else
+            {
+                swap(nums[k], nums[j]);
+                j--;
+            }
+        }
+    }
+};
