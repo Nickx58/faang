cdb48ba23d29142b1f324fb5100b86ae534c4753
Nickx58 <nikhilx58@gmail.com>
2026-02-25 09:11:34 +0530

    Happy Number and added folder for patterns

---
+++ ds/patterns/fastSlowPointer/happyNumber.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    bool isHappy(int n)
+    {
+        int slow = getNextNumber(n);
+        int fast = getNextNumber(getNextNumber(n));
+
+        while (slow != fast)
+        {
+            if (fast == 1)
+            {
+                return true;
+            }
+            slow = getNextNumber(slow);
+            fast = getNextNumber(getNextNumber(fast));
+        }
+        return slow == 1;
+    }
+    int getNextNumber(int n)
+    {
+        int sum = 0;
+        while (n > 0)
+        {
+            int digit = n % 10;
+            sum += digit * digit;
+            n = n / 10;
+        }
+        return sum;
+    }
+};
