be8ca722a8caa04feb44c0e1c8a16fb191f4af08
Nickx58 <nikhilx58@gmail.com>
2026-03-03 21:48:14 +0530

    Max sum of twin linked list

--- ds/patterns/fastSlowPointer/maxTwinSumLinkedList.cpp
+++ ds/patterns/fastSlowPointer/maxTwinSumLinkedList.cpp
@@ -37,3 +37,48 @@ public:
         return maxi;
     }
 };
+
+/*
+    Using fast and slow pointer
+    1. Reach the middle of the linked list
+    2. Reverse the linked list after the middle point
+    3. Use two pointer for sum
+*/
+
+class Solution
+{
+public:
+    int pairSum(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode *prev = nullptr;
+
+        while (slow)
+        {
+            ListNode *nextNode = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        ListNode *first = head;
+        ListNode *second = prev;
+        int maxi = INT_MIN;
+        while (first && second)
+        {
+            int sum = first->val + second->val;
+            maxi = max(maxi, sum);
+            first = first->next;
+            second = second->next;
+        }
+        return maxi;
+    }
+};
