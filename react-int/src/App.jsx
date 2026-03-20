adb78d32889876c97c1307b2bd2c36132f2ea063
Nickx58 <nikhilx58@gmail.com>
2026-03-20 23:52:06 +0530

    Clean Up tab form component

--- react-int/src/App.jsx
+++ react-int/src/App.jsx
@@ -10,7 +10,6 @@ import PinItems from "./components/PinItems/PinItems";
 import OTP from "./components/OTP/OTP";
 import SearchInput from "./components/SearchInput/SearchInput";
 import { ToastProvider } from "./components/Toast/ToastProvider";
-import TabForm from "./components/TabForm/TabForm";
 
 function App() {
   return (
@@ -27,7 +26,6 @@ function App() {
           <Tabs.Trigger value="tab8">Pin Items</Tabs.Trigger>
           <Tabs.Trigger value="tab9">OTP</Tabs.Trigger>
           <Tabs.Trigger value="tab10">Search Input</Tabs.Trigger>
-          <Tabs.Trigger value="tab_11">Tab Form</Tabs.Trigger>
         </Tabs.List>
         <Tabs.Content value="tab1">
           <Todo />
@@ -58,9 +56,6 @@ function App() {
         </Tabs.Content>
         <Tabs.Content value="tab10">
           <SearchInput />
-        </Tabs.Content>
-        <Tabs.Content value="tab_11">
-          <TabForm />
         </Tabs.Content>
       </Tabs>
     </ToastProvider>
--- react-int/src/components/TabForm/Interest.jsx
+++
@@ -1,9 +0,0 @@
-function Interest() {
-  return (
-    <div>
-      <h2>Interest</h2>
-    </div>
-  );
-}
-
-export default Interest;
