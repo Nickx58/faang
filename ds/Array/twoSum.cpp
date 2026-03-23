8b123db26603e3df44f1d1b6e738721597e782c4
Nickx58 <nikhilx58@gmail.com>
2026-03-23 08:57:56 +0530

    Array: Two Sum

---
+++ ds/Array/twoSum.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> hash;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int com = target - nums[i];
+            if (hash.find(com) != hash.end())
+            {
+                return {hash[com], i};
+            }
+            hash[nums[i]] = i;
+        }
+        return {};
+    }
+};
