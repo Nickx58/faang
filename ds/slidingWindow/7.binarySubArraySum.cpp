fab95460568577ece01ea20c6b3fc64f80ffa481
Nickx58 <nikhilx58@gmail.com>
2026-07-29 13:20:29 +0530

    Binary SubArray Sum

---
+++ ds/slidingWindow/7.binarySubArraySum.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    int atMostGoal(vector<int> &nums, int goal)
+    {
+        if (goal < 0)
+        {
+            return 0;
+        }
+        int left = 0;
+        int sum = 0;
+        int count = 0;
+        for (int right = 0; right < nums.size(); right++)
+        {
+            sum += nums[right];
+            while (sum > goal)
+            {
+                sum -= nums[left];
+                left++;
+            }
+            count += (right - left + 1);
+        }
+        return count;
+    }
+    int numSubarraysWithSum(vector<int> &nums, int goal)
+    {
+        return atMostGoal(nums, goal) - atMostGoal(nums, goal - 1);
+    }
+};
